ce1ca6701e0eb20735cc9b3f0badf93955734fd3
Zac <zac.m@timetabling.com.au>
2019-08-12 11:06:04 +1000

    Added waitforelement

--- USAT.js
+++ USAT.js
@@ -6,4 +6,23 @@ function qs(selector) {
 	return document.querySelector(selector);
 }
 
-function 
+async function waitForElement(selector, timeout, period) {
+	var promise = new Promise(function(resolve, reject) {
+		let period = period || 200;
+		let loopIdx = 0;
+		let checkElem = setInterval(function() {
+			let elem = qs(selector);
+			if (elem != null) {
+				resolve(elem);
+				clearInterval(checkElem);
+			} else {
+				if (timeout && (period * loopIdx >= timeout)) {
+					reject();
+					clearInterval(checkElem);
+				}
+				loopIdx++;
+			}
+		}, period);
+	});
+	return promise;
+}
